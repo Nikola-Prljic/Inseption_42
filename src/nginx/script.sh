6464b28847c9467a163bc810fee0bdd07bdf683a
Nikola-Prljic <72382235+Nikola-Prljic@users.noreply.github.com>
2023-10-24 21:46:43 +0200

    Delete src directory

--- src/nginx/script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /cert/nginx-selfsigned.pem -subj "/C=AT/L=VIENNA/O=1337/OU=student/CN=nprljic.42.at"
